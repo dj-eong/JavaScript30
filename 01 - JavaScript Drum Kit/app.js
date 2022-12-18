877bb8259e52cdd2f420bebadb827c1109e053fd
David Jeong <davidjeong221@gmail.com>
2022-12-17 19:53:53 -0500

    added js file

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,30 @@
+// const keyArray = document.querySelectorAll('.key');
+// const soundsArray = document.querySelectorAll('audio');
+
+function playSound(note) {
+    clone = note.cloneNode();
+    clone.play();
+}
+
+// window.addEventListener('keydown', function (e) {
+//     if (e.keyCode === 65) {
+//         keyArray[0].classList.add('playing');
+//         playSound(soundsArray[0]);
+//         this.setTimeout(function () {
+//             keyArray[0].classList.remove('playing');
+//         }, 100);
+//     }
+// });
+
+window.addEventListener('keydown', function (e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!key) return;
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+
+    this.setTimeout(function () {
+        key.classList.remove('playing');
+    }, 100);
+
+    playSound(sound);
+});
